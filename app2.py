fc07870b69a55fd4dc3a39c908e6347a5944c5d0
Taher <taherhossieninasab@gmail.com>
2024-09-11 13:09:14 +0330

    Initial commit

--- app2.py
+++ app2.py
@@ -38,4 +38,4 @@ def analyze_sentiment():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=10000)
